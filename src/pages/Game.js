dbe3d6f95377acad6379f9408d6406672c98cb7c
sbevening <91366243+sbevening@users.noreply.github.com>
2023-07-12 00:38:32 -0700

    Random Track Select Implemented
    Added function to select random track from a list of Spotify tracks/songs.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,12 +5,10 @@ function Game() {
   const token = window.localStorage.getItem("token");
   const [topTracks, setTopTracks] = useState([]);
 
-  console.log(token);
-
   useEffect(() => {
-    const GetTopTracks = async () => {
+    const GetTopTracks = async () => { // must be declared inside useEffect for async to work
       const { data } = await axios.get(
-        "https://api.spotify.com/v1/me/top/tracks?limit=10",
+        "https://api.spotify.com/v1/me/top/tracks?limit=50",
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -25,8 +23,23 @@ function Game() {
     GetTopTracks();
   }, [token]); // Get top tracks as soon as token is generated and assigned
 
-  return (
-    (topTracks.length > 0) ? <h1>Yay</h1> : <h1>Nay</h1> // Placeholders
+  const SelectRandomTrack = () => {
+    if (topTracks.length > 0) {
+      const randIndex = Math.floor(Math.random() * topTracks.length);
+      const randTrack = topTracks[randIndex];
+      return randTrack;
+    }
+  }
+
+  if (topTracks.length < 1) {
+    return <h1>Hold up.</h1>
+  }
+  else return (
+    <div>
+      {
+        SelectRandomTrack().name
+      }
+    </div>
   )
 }
 
