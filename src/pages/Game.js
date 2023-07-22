d1fafe7cfbae247d1241519d83914aae9f5799ef
sbevening <91366243+sbevening@users.noreply.github.com>
2023-07-22 00:36:36 -0700

    Expired Token Error Handling + Shuffle Tracks Rework
    Added redirect and error handling for expired tokens.
    Shuffle tracks has been reworked to return all 50 top tracks in a random order.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "../UIComponents/Loading";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import shuffle from "just-shuffle";
 
 function Game() {
   const token = window.localStorage.getItem("token");
   const [topTracks, setTopTracks] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
+    if (token == null) return;
     const GetTopTracks = async () => {
       const { data } = await axios
         .get("https://api.spotify.com/v1/me/top/tracks?limit=50", {
@@ -16,28 +18,29 @@ function Game() {
             Authorization: `Bearer ${token}`,
           },
         })
-        .catch((err) => console.log(err));
-
-      setTopTracks(data.items);
+        .catch((err) => {
+          if (err.response) {
+          window.localStorage.clear();
+          window.location.reload();
+          }
+        });
+      
+      const tracks = data.items;
+      const shuffledTracks = shuffle(tracks);
+      setTopTracks(shuffledTracks);
     };
     GetTopTracks();
   }, []);
 
-  if (token == null) {
-    return <Navigate to="/auth" />;
-  }
+  if (token == null) return <Navigate to="/auth" />;
 
   if (topTracks.length < 1) {
     return <Loading />;
   } else {
-    const shuffledTracks = shuffle(topTracks); // randomizes order of topTracks
-    const selectedTracks = shuffledTracks.slice(-5); // gets last 5 tracks of randomized order
-    console.log(selectedTracks)
-
     return (
       <div>
         <ul>
-          {selectedTracks.map((track, i) => <li key={i}>{track.name}</li>)}
+          {topTracks.map((track, i) => <li key={i}>{track.name}</li>)}
         </ul>
       </div>
     );
