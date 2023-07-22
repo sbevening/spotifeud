ccf2d0510aca7f06c201f642696da0bea3d517f8
sbevening <91366243+sbevening@users.noreply.github.com>
2023-07-21 23:37:54 -0700

    Get Top Songs Refinement
    Fixed extra calls being made to function fetching user's top tracks from Spotify API by disabling strict mode.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,52 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Loading from "../UIComponents/Loading"
+import Loading from "../UIComponents/Loading";
 import { Navigate } from "react-router-dom";
+import shuffle from "just-shuffle";
 
 function Game() {
   const token = window.localStorage.getItem("token");
   const [topTracks, setTopTracks] = useState([]);
 
   useEffect(() => {
-    const GetTopTracks = async () => { // must be declared inside useEffect for async to work
-      const { data } = await axios.get(
-        "https://api.spotify.com/v1/me/top/tracks?limit=50",
-        {
+    const GetTopTracks = async () => {
+      const { data } = await axios
+        .get("https://api.spotify.com/v1/me/top/tracks?limit=50", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        }
-      )
-      .catch(
-          err => console.log(err)
-      );
-      if (data) setTopTracks(data.items);
+        })
+        .catch((err) => console.log(err));
+
+      setTopTracks(data.items);
     };
     GetTopTracks();
-  }, [token]); // Get top tracks as soon as token is generated and assigned
-
-  const SelectRandomTrack = () => {
-    if (topTracks.length > 0) {
-      const randIndex = Math.floor(Math.random() * topTracks.length);
-      const randTrack = topTracks[randIndex];
-      return randTrack;
-    }
-  }
+  }, []);
 
   if (token == null) {
-    return <Navigate to="/auth" />
+    return <Navigate to="/auth" />;
   }
-  
+
   if (topTracks.length < 1) {
-    return <Loading />
+    return <Loading />;
+  } else {
+    const shuffledTracks = shuffle(topTracks); // randomizes order of topTracks
+    const selectedTracks = shuffledTracks.slice(-5); // gets last 5 tracks of randomized order
+    console.log(selectedTracks)
+
+    return (
+      <div>
+        <ul>
+          {selectedTracks.map((track, i) => <li key={i}>{track.name}</li>)}
+        </ul>
+      </div>
+    );
   }
-  else return (
-    <div>
-      {
-        SelectRandomTrack().name
-      }
-    </div>
-  )
 }
 
 export default Game;
